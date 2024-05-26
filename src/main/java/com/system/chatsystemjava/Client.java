e2f22d1b1a7df21467a3328235eadfe69a682821
maximilian.schulz2006@outlook.de <maximilian.schulz2006@outlook.de>
2024-05-26 21:48:40 +0200

    Divided server and client into their own parts -> makes it simpler to add multiple connections at once

--- src/main/java/com/system/chatsystemjava/Client.java
+++ src/main/java/com/system/chatsystemjava/Client.java
@@ -50,7 +50,7 @@ public class Client implements Runnable{
         }
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
        Thread clientThread = new Thread(new Client("Max"));
 
        clientThread.start();
